dd114bb8f2ddf0e8d140350b9b3c117aabe355c3
Marcin Lewandowski <marcin@ravendb.net>
2017-11-04 13:00:51 +0100

    RavenDB-9218 Setup app UI

--- src/Raven.Server/Json/JsonDeserializationServer.cs
+++ src/Raven.Server/Json/JsonDeserializationServer.cs
@@ -107,6 +107,8 @@ namespace Raven.Server.Json
         public static readonly Func<BlittableJsonReaderObject, UnsecuredSetupInfo> UnsecuredSetupInfo = GenerateJsonDeserializationRoutine<UnsecuredSetupInfo>();
 
         public static readonly Func<BlittableJsonReaderObject, ClaimDomainInfo> ClaimDomainInfo = GenerateJsonDeserializationRoutine<ClaimDomainInfo>();
+        
+        public static readonly Func<BlittableJsonReaderObject, ListDomainsInfo> ListDomainsInfo = GenerateJsonDeserializationRoutine<ListDomainsInfo>();
 
         public static readonly Func<BlittableJsonReaderObject, RestoreSettings> RestoreSettings = GenerateJsonDeserializationRoutine<RestoreSettings>();
 
--- src/Raven.Studio/typings/_studio/dto.d.ts
+++ src/Raven.Studio/typings/_studio/dto.d.ts
@@ -382,7 +382,7 @@ type dashboardChartTooltipProviderArgs = {
     values: dictionary<number>;
 }
 
-interface claimDomainResult {
+interface registrationInfoResult {
     Email: string;
     Domains: dictionary<Array<string>>;
 }
