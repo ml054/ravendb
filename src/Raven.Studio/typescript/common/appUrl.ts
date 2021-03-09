e8aa3f87f7cc91e629f7c64ce3d4175c2bc0bb2e
ml054 <marcin@ravendb.net>
2021-03-09 21:26:52 +0100

    RavenDB-14163 Cluster dashboard - architecture

--- src/Raven.Studio/typescript/common/appUrl.ts
+++ src/Raven.Studio/typescript/common/appUrl.ts
@@ -209,6 +209,10 @@ class appUrl {
     static forServerDashboard(): string {
         return "#dashboard";
     }
+    
+    static forClusterDashboard(): string {
+        return "#clusterDashboard";
+    }
 
     static forEditCmpXchg(key: string, db: database | databaseInfo) {
         const databaseUrlPart = appUrl.getEncodedDbPart(db);
