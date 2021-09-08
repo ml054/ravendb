6d906c2770f3ed88c3ce68caa07dd26a766f4a6e
ml054 <marcin@ravendb.net>
2021-09-08 11:25:19 +0200

    RavenDB-15750 Take leverage from ANTLR and use it for intellisense in Studio

---
+++ src/Raven.Studio/languageService/test/autocompleteUtils.ts
@@ -0,0 +1,26 @@
+import { handleAutoComplete } from "../src/autocomplete";
+import { CaretPosition } from "../src/types";
+
+const caret = "|";
+
+export function autocomplete(input: string): Promise<autoCompleteWordList[]> {
+    const caretIdx = input.indexOf(caret);
+    if (caretIdx === -1) {
+        throw new Error("Unable to find caret (|) in input string: " + input);
+    }
+    
+    const lines = input.split(/\r?\n/);
+    const row = lines.findIndex(x => x.includes(caret));
+    const column = lines[row].indexOf("|");
+
+    const mappedLines = lines
+        .map((line, idx) => idx === row ? line.replace("|", "") : line);
+    
+    const inputWithoutCaret = mappedLines.join("\r\n");
+    const caretPosition: CaretPosition = {
+        column,
+        line: row + 1
+    };
+    
+    return handleAutoComplete(inputWithoutCaret, caretPosition);
+}
