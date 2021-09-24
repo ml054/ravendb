3dcef99707a0564c336e5f1a909dffd4a4523a70
ml054 <marcin@ravendb.net>
2021-09-24 09:23:11 +0200

    RavenDB-12083 Migrate Studio build to webpack

--- src/Raven.Studio/typescript/viewmodels/manage/captureStackTraces.ts
+++ src/Raven.Studio/typescript/viewmodels/manage/captureStackTraces.ts
@@ -71,6 +71,8 @@ class stackInfo {
 
 class captureStackTraces extends viewModelBase {
     
+    view = require("views/manage/captureStackTraces.html");
+    
     spinners = {
         loading: ko.observable<boolean>(false)
     };
